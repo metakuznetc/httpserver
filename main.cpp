0fa66db8aedb6c4b403261586c539151a61cf3f4
metakuznetc <66172535+metakuznetc@users.noreply.github.com>
2023-06-10 12:21:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,8 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-
-    // Создание экземпляра сервера
     HttpServer server;
 
-    // Попытка запуска сервера на порту 8080
     if (!server.listen(QHostAddress::Any, 8080)) {
         qDebug() << "Failed to start server.";
         return -1;
